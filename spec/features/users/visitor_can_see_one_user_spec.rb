7fb6b962585f5e3817397d8f56ff2374457c04e4
Patrick Shobe <patrick@patrickshobe.com>
2018-10-09 11:55:01 -0600

    Adds mocks

--- spec/features/users/visitor_can_see_one_user_spec.rb
+++ spec/features/users/visitor_can_see_one_user_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 feature 'User Show' do
   context 'As a visitor' do
     scenario 'visits the user show page' do
-      user = create(:user)
-      visit '/users/1'
+      VCR.use_cassette("user") do
+        user = create(:user)
+        visit '/users/1'
 
-      expect(page).to have_content(user.name)
+        expect(page).to have_content(user.name)
+      end
     end
   end
 end
