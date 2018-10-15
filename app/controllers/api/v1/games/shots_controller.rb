c64ea88d8da3db563221a4817bdfc2c34b49e04a
abdyqew <aqudrat630@gmail,com>
2018-10-15 14:23:08 -0600

    adds Invalid Coordinate logic, spec harness pass until line 116

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -7,11 +7,19 @@ module Api
           if game.current_turn == "player_1" && request.headers["X-API-Key"] == game.player_1_api_key
             turn_processor = TurnProcessor.new(game, params[:shot][:target], request.headers["X-API-Key"])
             turn_processor.run!
-            render json: game, message: turn_processor.message
+            if turn_processor.message.include?("Invalid")
+              render json: game, message: turn_processor.message, status: 400
+            else
+              render json: game, message: turn_processor.message
+            end
           elsif game.current_turn == "player_2" && request.headers["X-API-Key"] != game.player_1_api_key
             turn_processor = TurnProcessor.new(game, params[:shot][:target], request.headers["X-API-Key"])
             turn_processor.run!
-            render json: game, message: turn_processor.message
+            if turn_processor.message.include?("Invalid")
+              render json: game, message: turn_processor.message, status: 400
+            else
+              render json: game, message: turn_processor.message
+            end
           else
             render json: game, status: 400, message: "Invalid move. It's your opponent's turn"
           end
